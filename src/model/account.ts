8b49a7e2e66635927cfd9b69a96ee23f01c9eade
Jason <>
2023-11-12 23:04:08 +0800

    账户功能完善

--- src/model/account.ts
+++ src/model/account.ts
@@ -1,32 +1,40 @@
-import {Account as AleoAccount, PrivateKey} from '@aleohq/sdk';
+import {Account as AleoAccount, Signature} from '@aleohq/sdk';
 
 
 // privateKey:  APrivateKey1zkpEFHotKw8L8eFwVgSEzCAAragxZ3pejRGeUb8R9jVpqJX
 // viewKey:  AViewKey1d4N8Nz8mKJKG8b6orn2NLyJLNQRotRwCweBuAxnTK3XW
 // address:  aleo1fma3l3ygpmqf7629c5rg0hdd0j20g7lhfvav9qyrlsj6mqmr9sys0h2qup
 export class Account {
-    privateKey: string
-    viewKey: string
-    address: string
+    private aleoAccount: AleoAccount
 
     constructor();
     constructor(privateKey: string);
 
     constructor(privateKey?: string) {
-        let address: string = ''
-        let viewKey: string = ''
-        if (privateKey !== undefined) {
-            let pk = PrivateKey.from_string(privateKey)
-            address = pk.to_address().to_string()
-            viewKey = pk.to_view_key().to_string()
-        } else {
-            const aleoAccount = new AleoAccount();
-            privateKey = aleoAccount.privateKey().to_string();
-            viewKey = aleoAccount.viewKey().to_string();
-            address = aleoAccount.address().to_string();
-        }
-        this.privateKey = privateKey
-        this.viewKey = viewKey
-        this.address = address
+        this.aleoAccount = new AleoAccount({privateKey: privateKey})
+    }
+
+    decryptRecord(ciphertext: string): string {
+        return this.aleoAccount.decryptRecord(ciphertext)
+    }
+
+    sign(msg: number[]): Signature {
+        return this.aleoAccount.sign(Uint8Array.from(msg))
+    }
+
+    verify(msg: number[], signature: Signature): boolean {
+        return this.aleoAccount.verify(Uint8Array.from(msg), signature)
+    }
+
+    get privateKey(): string {
+        return this.aleoAccount.privateKey().to_string()
+    }
+
+    get address(): string {
+        return this.aleoAccount.address().to_string()
+    }
+
+    get viewKey(): string {
+        return this.aleoAccount.viewKey().to_string()
     }
 }
